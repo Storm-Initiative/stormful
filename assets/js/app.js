c04f73d983ee1cf7ac376be1c0fe2ef2879e991f
spookyorange <82583304+spookyorange@users.noreply.github.com>
2024-10-07 22:34:04 +0300

    flyio first deploy!

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,7 +22,7 @@ import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 import Hooks from "./hooks"
-import Alpine from 'alpinejs'
+import 'alpinejs'
 window.Alpine = Alpine
 
 Alpine.start()
