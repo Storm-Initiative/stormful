5d22153a20bbc18d1c446dbdbfb8ed5f774e635f
spookyorange <82583304+spookyorange@users.noreply.github.com>
2024-10-10 22:03:11 +0300

    Revert "flyio first deploy!"
    This reverts commit c04f73d983ee1cf7ac376be1c0fe2ef2879e991f.

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,7 +22,7 @@ import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 import Hooks from "./hooks"
-import 'alpinejs'
+import Alpine from 'alpinejs'
 window.Alpine = Alpine
 
 Alpine.start()
--- rel/overlays/bin/migrate.bat
+++
@@ -1,1 +0,0 @@
-call "%~dp0\stormful" eval Stormful.Release.migrate
